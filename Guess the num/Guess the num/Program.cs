37b1113d6f30aea3e7e0d6e3962ac762d5b90e06
User <kenobi88@gmail.com>
2025-03-01 17:32:07 +0200

    1-commit

---
+++ Guess the num/Guess the num/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Welcome to guess the number!");
+Console.Write("Enter diapazon minimum: ");
+int min = int.Parse(Console.ReadLine());
+
+Console.Write("Enter diapazon maximum: ");
+int max = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Imagine a number from {min} to {max}, and I`ll try to guess it");
+Console.WriteLine("Press enter, when you`re ready.");
+Console.ReadLine();
+
+int guess;
+int attempts = 0;
+string response;
+
+do
+{
+    attempts++;
+    guess = (min + max) / 2;
+    Console.WriteLine($"My attempt: №{attempts}: {guess}");
+    Console.Write("Is this number more (>), less  (<) or equal (=) to yours? ");
+    response = Console.ReadLine();
+
+    if (response == ">")
+    {
+        max = guess - 1;
+    }
+    else if (response == "<")
+    {
+        min = guess + 1;
+    }
+    else if (response != "=")
+    {
+        Console.WriteLine("Please, type '>', '<' or '='.");
+    }
+
+} while (response != "=");
+
+Console.WriteLine($"Hehehe! I guessed the num after {attempts} tries!");
